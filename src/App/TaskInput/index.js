8c249a6301f4881c32f4429da8d34d5bae8a1b63
Jean-Philippe Hoareau Méavilla <jp@imac-de-jean-philippe.home>
2019-10-21 00:00:38 +0200

    Changing component to class. Adding self state/methods

--- src/App/TaskInput/index.js
+++ src/App/TaskInput/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import PlaylistAdd from '@material-ui/icons/PlaylistAdd';
 import IconButton from '@material-ui/core/IconButton';
@@ -7,27 +7,44 @@ import './TaskInput.css';
 
 // Fonction affichant la commande d'entrée de texte pour les tâches
 // Avec son bouton de validation
-// - L'attribut error ligne 18 de l'input permet le soulignement en rouge
-const TaskInput = ({ handleChange, handleSubmit, newTask }) =>
-  <form className="Form" onSubmit={handleSubmit}>
-    <Input
-      placeholder="Yeah, time to make your own quests"
-      onChange={handleChange('newTask')}
-      value={newTask}
-      className="Input"
-      error
-      required
-      multiline
-    />
-    <IconButton type="submit">
-      <PlaylistAdd className="Button" />
-    </IconButton>
-  </form>;
+// - L'attribut error ligne 35 de l'input permet le soulignement en rouge
+
+class TaskInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: "",
+    }
+  }
+  handleChange = e => {
+    this.setState({message: e.target.value});
+  };
+  handleS = e => {
+    e.preventDefault();
+    if (this.state.message.trim().length > 0)
+      this.props.addTask({...this.state, state: true});
+  }
+  render () {
+    return (
+      <form className="Form" onSubmit={this.handleS}>
+        <Input
+          placeholder="Yeah, time to make your own quests"
+          onChange={this.handleChange}
+          value={this.state.newTask}
+          className="Input"
+          error
+          required
+          multiline
+        />
+        <IconButton type="submit">
+          <PlaylistAdd className="Button" />
+        </IconButton>
+      </form>)
+  }
+}
 
 TaskInput.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  newTask: PropTypes.string,
+  addTask: PropTypes.func.isRequired,
 };
 
 export default TaskInput;
