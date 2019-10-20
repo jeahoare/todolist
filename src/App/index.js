84029d8b2b06b3508ad8c3bb88e0899936779999
Jean-Philippe Hoareau Méavilla <jp@imac-de-jean-philippe.home>
2019-10-21 00:02:47 +0200

    Changing and delegating some methods

--- src/App/index.js
+++ src/App/index.js
@@ -4,10 +4,7 @@ import TaskInput from './TaskInput';
 import TasksList from './TasksList';
 import './App.css';
 
-// Application d'une To Do List :
-// - Création de tâche
-// - Validation de tâche
-// - Suppression de tâche
+// Application d'une To Do List (Création/Validation/Suppression) de tâche
 class App extends Component {
   constructor(props) {
     super(props);
@@ -16,52 +13,37 @@ class App extends Component {
       tasks: [],
     }
   }
-  // Méthode permettant de mettre à jour l'attribut de NewTask de l'input
-  handleChange = name => event => {
-    this.setState({[name]: event.target.value});
-  };
   // Méthode ajoutant une tâche à la liste si elle n'est pas vide
-  handleSubmit = event => {
-    if (this.state.newTask.trim().length > 0) {
-      let tasksTmp = this.state.tasks;
-      tasksTmp.push({
-        message: this.state.newTask,
-        state: true,
-      });
-      this.setState({tasks: tasksTmp});
-    }
-    event.preventDefault();
+  addTask = task => {
+    let tasksTmp = [...this.state.tasks, task];
+    this.setState({tasks: tasksTmp});
   };
   // Méthode supprimant la tâche associée
-  deleteTask = index => event => {
+  deleteTask = index => {
     let tasksTmp = this.state.tasks;
     tasksTmp.splice(index, 1);
     this.setState({tasks: tasksTmp});
   }
-  // Méthode qui permet de modifier le contenue
-  // d'une tâche de la liste lorsqu'elle est cliquée
+  // Méthode permettant d'éditer le contenue d'une tâche
   editTask = index => event => {
     let tasksTmp = this.state.tasks;
     tasksTmp[index].message = event.target.value;
     this.setState({tasks: tasksTmp});
   }
-  // Méthode permettant de change l'état d'une tâche entre validée ou non
-  // Son apparence change en fonction. 
-  stateTask = index => event => {
+  // Méthode permettant de changer l'état d'une tâche entre validée ou non
+  stateTask = index => {
     let tasksTmp = this.state.tasks
     tasksTmp[index].state = tasksTmp[index].state ? false : true;
     this.setState({tasks: tasksTmp});
   }
   render () {
-    const { task, tasks } = this.state;
+    const { tasks } = this.state;
     return (
       <div className="App">
         <div className="ToDoList">
           <Header />
           <TaskInput 
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-            task={task}
+            addTask={this.addTask}
           />
           <TasksList
             tasks={tasks}
