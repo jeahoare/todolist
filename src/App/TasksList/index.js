656fddf46164d54a8194fdadb41a19e02f043297
Jean-Philippe Hoareau Méavilla <jp@imac-de-jean-philippe.home>
2019-10-23 15:54:04 +0200

    Changing css for validated task and adding ternary

--- src/App/TasksList/index.js
+++ src/App/TasksList/index.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
 import Delete from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
-// import { makeStyles } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
 import { deleteTask, stateTask, editTask } from '../../actions/taskActions';
 import './tasksList.css';
 
-/* Apparence du message si la tâche est validée  */
-/* Permet de modifier les composants material-ui */
-// const useStyles = makeStyles({
-//   root: {
-//     color: '#80808026',
-//     alignSelf: 'center',
-//     textAlign: 'justify',
-//     overflow: 'auto',
-//     flexGrow: '1',
-//     marginLeft: '10px',
-//     textDecoration: 'line-through',
-//   },
-// });
-
 // Fonction permettant l'affichage des tâches une à une, possibilité de
 // validation et suppression. Leur état de validation définit leur 
-// apparence barée ou non selon leur state.
+// apparence grisée ou non selon leur state.
 class TasksList extends Component {
   deleteTask = index => {
     this.props.deleteTask(index);
@@ -43,42 +28,40 @@ class TasksList extends Component {
     this.props.stateTask(tasksTmp);
     this.forceUpdate()
   }
-  printTaskByState = ( bool ) => {
-    return this.props.tasks.map((task, index) => {
-      let state = task.state;
-      return (state === bool) ?
-        (<div key={index} className="Task">
-          <Checkbox
-            checked={!state}
-            onClick={() => this.stateTask(index)}
-            inputProps={{'aria-label': 'checkbox with default color'}}
-          />
-          <Input 
-            className={state ? "Message" : /*classes.root*/null}
-            value={task.message}
-            onChange={(e) => this.editTask(index, e)}
-            multiline
-            disableUnderline
-          />
-          <div
-            className={state ? "Delete" : "DelCross"}
-            onClick={() => this.deleteTask(index)}
-          >
-            <Delete />
-          </div>
-        </div>) : null;
-    })
-  }
   render() {
-   // const classes = useStyles();
-   if (this.props.tasks.length === 0)
-     return <span>Let's achieve something !</span>;
-    return (
-      <div>
-        {this.printTaskByState(true)}
-        {this.printTaskByState(false)}
-      </div>
-    );
+    const printTaskByState = ( bool ) => {
+      return this.props.tasks.map((task, index) => {
+        let state = task.state;
+        return (state === bool) ?
+          (<div key={index} className="Task">
+            <Checkbox
+              checked={!state}
+              onClick={() => this.stateTask(index)}
+              inputProps={{'aria-label': 'checkbox with default color'}}
+            />
+            <Input 
+              className={state ? "Message" : "Message"}
+              value={task.message}
+              onChange={(e) => this.editTask(index, e)}
+              multiline
+              disableUnderline
+              disabled={!state}
+            />
+            <div
+              className={state ? "Delete" : "DelCross"}
+              onClick={() => this.deleteTask(index)}
+            >
+              <Delete />
+            </div>
+          </div>) : null;
+      })
+    }
+    return (this.props.tasks.length === 0) ?
+      (<span>Let's achieve something !</span>)
+      : (<div>
+        {printTaskByState(true)}
+        {printTaskByState(false)}
+      </div>);
   }
 }
 
